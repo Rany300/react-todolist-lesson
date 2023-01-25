b4908090720edd3677e22769cd429830db127df2
Daniel <76593188+Rany300@users.noreply.github.com>
2023-01-25 17:12:29 +0100

    !Finished TODOList Basic +Added minimal styling

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import "./TodoListBasic.css";
 const types: string[] = ["to do", "in progress", "done"];
 
 type TODO = {
@@ -7,18 +7,18 @@ type TODO = {
     type: typeof types[number];
 }
 
-
 const TodoListBasic = () => {
     const [todos, setTodos] = useState<TODO[]>([]);
-    const [currentTodo, setCurrentTodo] = useState<TODO>({
+    const [newTodo, setNewTodo] = useState<TODO>({
       content : "",
       type: types[0]
     });
     
+    
     const addTodo = (todo: TODO) => {
       if (todo.content.length > 0){
           setTodos([...todos, todo]);
-          setCurrentTodo({
+          setNewTodo({
             content: "",
             type: "to do"
           });
@@ -26,19 +26,19 @@ const TodoListBasic = () => {
     };
   
     const todoList = todos.map((todo, i) => 
-    (
-      <div key={i} style={{display: "flex", gap: "10px"}}>
-          <div>{todo.content}</div>
-          <div style={{fontWeight: "bold"}}>{todo.type}</div>
-      </div>
-    ));
+  (
+    <div key={i} className="todo-item">
+        <div>{todo.content}</div>
+        <div className="todo-content">{todo.type}</div>
+    </div>
+  ));
     const addTodoForm = (
       <div>
-        <input type="text" value={currentTodo.content} onChange={(e) => setCurrentTodo({...currentTodo,content: e.target.value}) } placeholder="Enter task"></input>
-        <select value={currentTodo.type} onChange={(e) => setCurrentTodo({...currentTodo, type: e.target.value})}>
+        <input type="text" value={newTodo.content} onChange={(e) => setNewTodo({...newTodo,content: e.target.value}) } placeholder="Enter task"></input>
+        <select value={newTodo.type} onChange={(e) => setNewTodo({...newTodo, type: e.target.value})}>
           {types.map((type, i) => <option key={i} value={type}>{type}</option>)}
         </select>
-        <button onClick={() => addTodo(currentTodo)}>Add TODO</button>
+        <button onClick={() => addTodo(newTodo)}>Add TODO</button>
       </div>
     );
     return (
